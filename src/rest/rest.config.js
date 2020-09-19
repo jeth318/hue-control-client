e992827703077f9575f9c589d9b2a847576072e5
jeth318 <jesper.thornberg@me.com>
2020-09-19 16:56:08 +0200

    Fix paths

--- src/rest/rest.config.js
+++ src/rest/rest.config.js
@@ -5,7 +5,7 @@ config();
 const host = process.env.VUE_APP_API_HOST;
 const protocol = process.env.VUE_APP_PROTOCOL;
 const port = process.env.VUE_APP_PORT;
-export const baseUrl = `${protocol}://${host}:${port}`;
+export const baseUrl = `${protocol}://${host}:${port}/lights/api`;
 export const endpoints = {
     LIGHTS: 'lights',
     GROUPS: 'groups',
