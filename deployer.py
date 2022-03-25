fc87cad7c6d4faa23fe86b1d5722523ec60d4bc8
jeth318 <jesper.thornberg@me.com>
2022-03-25 11:24:14 +0100

    Test

--- deployer.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-subprocess.run(["scp", "-r", "-P", "2244", "/Users/jesper/dev/hue-control/client/dist/",
-               "jeth@10.0.128.110:/home/jeth/apps/skojj"])
-
-print("Done")
-
