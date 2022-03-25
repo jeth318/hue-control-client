7973251e0012d5f2f529515d932917c362f371e1
jeth318 <jesper.thornberg@me.com>
2022-03-25 09:38:07 +0100

    Refactor

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,3 @@
-const path = require('path')
-const isLocalDev = () => process.env.VUE_APP_LOCALDEV
-const isProductionBuild = () => process.env.NODE_ENV === 'production'
-
-const getPublicPath = () => {
-  if (isProductionBuild()) {
-      if (isLocalDev()) {
-          return path.resolve(__dirname, 'dist')
-      }
-      return '/lights'
-  }
-  return '/'
-}
-
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  publicPath: getPublicPath()
-}
+    transpileDependencies: ['vuetify'],
+};
